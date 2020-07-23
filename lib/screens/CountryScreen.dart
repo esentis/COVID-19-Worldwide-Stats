170c776e0074af6db40dffb4b4fede26522ca49a
esentis <esentakos@yahoo.gr>
2020-07-23 18:24:03 +0300

    Fixed chart functionality
    Chart is now show 6 different dates, forming a representative timeline of the virus.

--- lib/screens/CountryScreen.dart
+++ lib/screens/CountryScreen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -17,6 +18,16 @@ bool showSpinner = true;
 String countryCode = '';
 List<String> resultsList = ['', '', '', ''];
 List arguments = ['', '', ''];
+List dates = [
+  '2020-3-1',
+  '2020-4-15',
+  '2020-5-1',
+  '2020-5-15',
+  '2020-6-1',
+  '2020-6-10',
+  '2020-6-18',
+];
+List<double> casesByDate = [0, 0, 0, 0, 0, 0, 0];
 
 class _CountryScreenState extends State<CountryScreen> {
   VirusData virusData = new VirusData();
@@ -31,10 +42,6 @@ class _CountryScreenState extends State<CountryScreen> {
         jsonDecode(searchedCountryResults)[0]['recovered'].toString();
     var critical = jsonDecode(searchedCountryResults)[0]['critical'].toString();
     var deaths = jsonDecode(searchedCountryResults)[0]['deaths'].toString();
-    print("CONFIRMED CASES $confirmedCases");
-    print("RECOVERED CASES $recovered");
-    print("CRITICAL CASES $critical");
-    print("DEATHS $deaths");
     results = [confirmedCases, recovered, critical, deaths];
     resultsList = results;
     setState(() {
@@ -44,39 +51,19 @@ class _CountryScreenState extends State<CountryScreen> {
 
   Future<void> getCountryReport() async {
     //YYYY-MM-DD
-    var firstDate = "2020-2-1";
-    var response =
-        await virusData.dailyReportByCountryCode(countryCode, firstDate);
-    print(response);
-    print(jsonDecode(response)[0]['provinces'][0]['confirmed']);
-//    FlatButton(
-//        onPressed: () {
-//          DatePicker.showDatePicker(context,
-//              showTitleActions: true,
-//              minTime: DateTime(2020, 2, 5),
-//              maxTime: DateTime(2020, 6, 7),
-//              onChanged: (date) {
-//                print('change $date');
-//              }, onConfirm: (date) async {
-//                print('confirm $date');
-//
-//                //YYYY-MM-DD
-//                var firstDate =
-//                    "${date.year}-${date.month}-${date.day}";
-//                var response =
-//                await virusData.dailyReportByCountryCode(
-//                    countryCode, firstDate);
-//                print(response);
-//                print(
-//                    "The country is ${countryCode} and on $firstDate it had ${jsonDecode(response)[0]['provinces'][0]['confirmed'] ?? "zero"} confirmed cases ");
-//              },
-//              currentTime: DateTime.now(),
-//              locale: LocaleType.en);
-//        },
-//        child: Text(
-//          'Date Picker',
-//          style: TextStyle(color: Colors.blue),
-//        )),
+    for (var i = 0; i < dates.length; i++) {
+      var response =
+      await virusData.dailyReportByCountryCode(countryCode, dates[i]);
+      var active = jsonDecode(response)[0]['provinces'][0]['active'];
+      print(active);
+      if (active == null) {
+        casesByDate[i] = 0.toDouble();
+      } else {
+        casesByDate[i] = active.toDouble();
+      }
+      print(casesByDate);
+    }
+    setState(() {});
   }
 
   @override
@@ -101,76 +88,118 @@ class _CountryScreenState extends State<CountryScreen> {
                   Text(
                     arguments[2],
                     style: GoogleFonts.gfsNeohellenic(
-                        fontSize: 30, color: Colors.white),
+                        fontSize: 35, color: Colors.white),
                   ),
                   SizedBox(
                     height: 5,
                   ),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(160),
-                    child: Image.asset(
-                      arguments[1],
-                      package: 'country_list_pick',
-                      scale: 1.5,
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(160),
+                        child: Image.asset(
+                          arguments[1],
+                          package: 'country_list_pick',
+                          scale: 1.5,
+                        ),
+                      ),
+                      SizedBox(width: 5,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Text(
+                            "${resultsList[0].toString()} overall",
+                            style: GoogleFonts.gfsNeohellenic(
+                              fontSize: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            "${resultsList[1].toString()} recovered",
+                            style: GoogleFonts.gfsNeohellenic(
+                              fontSize: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            "${resultsList[2].toString()} critical",
+                            style: GoogleFonts.gfsNeohellenic(
+                              fontSize: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            "${resultsList[3].toString()} deaths",
+                            style: GoogleFonts.gfsNeohellenic(
+                              fontSize: 30,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                   SizedBox(
-                    height: 30,
+                    height: 40,
                   ),
-                  Column(
-                    children: [
-                      Text(
-                        "${resultsList[0].toString()} overall",
-                        style: GoogleFonts.gfsNeohellenic(
-                          fontSize: 30,
-                          color: Colors.white,
-                        ),
+                  LineChart(
+                    LineChartData(
+
+                      backgroundColor: Colors.transparent,
+                      axisTitleData: FlAxisTitleData(
+                        bottomTitle: AxisTitle(
+                            titleText: 'Dates',
+                            showTitle: true,
+                            textStyle:
+                            GoogleFonts.gfsNeohellenic(fontSize: 25)),
+                        leftTitle: AxisTitle(
+                            titleText: 'Active Cases',
+                            showTitle: true,
+                            textStyle:
+                            GoogleFonts.gfsNeohellenic(fontSize: 25)),
+                        topTitle: AxisTitle(
+                            titleText: 'Active Cases timeline',
+                            showTitle: true,
+                            textStyle:
+                            GoogleFonts.gfsNeohellenic(fontSize: 25)),
                       ),
-                      Text(
-                        "${resultsList[1].toString()} recovered",
-                        style: GoogleFonts.gfsNeohellenic(
-                          fontSize: 30,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "${resultsList[2].toString()} critical",
-                        style: GoogleFonts.gfsNeohellenic(
-                          fontSize: 30,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Text(
-                        "${resultsList[3].toString()} deaths",
-                        style: GoogleFonts.gfsNeohellenic(
-                          fontSize: 30,
-                          color: Colors.white,
-                        ),
-                      ),
-                      LineChart(
-                        LineChartData(
-                          backgroundColor: Colors.red.withOpacity(0.2),
-                          lineBarsData: [
-                            LineChartBarData(
-                                isCurved: true,
-                                barWidth: 3,
-                                spots: [
-                                  FlSpot(2, 2),
-                                  FlSpot(3, 1),
-                                  FlSpot(4, 4),
-                                  FlSpot(5, 3),
-                                ],
-                                aboveBarData: BarAreaData(
-                                    colors: [Colors.black, Colors.greenAccent]),
-                                curveSmoothness: 0.6,
-                                colors: [
-                                  Colors.blue,
-                                  Colors.yellow
-                                ])
+                      lineBarsData: [
+                        LineChartBarData(
+                          isCurved: true,
+                          show: true,
+                          curveSmoothness: 0.5,
+                          belowBarData: BarAreaData(
+                            show: true,
+                            colors: [
+                              Colors.blueGrey.withOpacity(0.5),
+                              Colors.red
+                            ],
+                          ),
+                          shadow: Shadow(
+                            color: Colors.white,
+                          ),
+                          barWidth: 3,
+                          spots: [
+                            FlSpot(1, casesByDate[0]),
+                            FlSpot(2, casesByDate[1]),
+                            FlSpot(3, casesByDate[2]),
+                            FlSpot(4, casesByDate[3]),
+                            FlSpot(5, casesByDate[4]),
+                            FlSpot(6, casesByDate[5]),
+                            FlSpot(7, casesByDate[6]),
                           ],
-                        ),
-                      ),
-                    ],
+                          preventCurveOverShooting: true,
+                          colors: [
+                            Colors.black.withOpacity(0.5),
+                            Colors.grey.withOpacity(0.5),
+                            Colors.red,
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ],
               ),

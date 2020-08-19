caeb86a1f534aff3f4919eb0b37f4e21c7d3f1cc
esentis <esentakos@yahoo.gr>
2020-08-19 17:20:02 +0300

    Adds Documentation

--- lib/virus_data.dart
+++ lib/virus_data.dart
@@ -5,27 +5,25 @@ import 'networking.dart';
 class VirusData {
   NetworkHelper networkOverallCases = NetworkHelper(url: totalsUrl);
 
-  //Method that calls API for overall cases data.
+  /// Returns overall cases.
   Future<dynamic> getOverallCases() async {
     var overallCasesData = await networkOverallCases.getData();
     return overallCasesData;
   }
 
-  //A methods that calls API for Cases by Country name
+  /// Returns cases using [country] name.
   Future<dynamic> getCasesByCountryName(String country) async {
-    var networkByCountry =
-        NetworkHelper(url: countryUrl + country);
+    var networkByCountry = NetworkHelper(url: countryUrl + country);
     var casesByCountryName = await networkByCountry.getData();
     return casesByCountryName;
   }
-  //Method for getting report for country on specific date
-  Future<String> dailyReportByCountryCode(
-      String countryName, String date) async {
+
+  /// Returns [country] report on specific [date].
+  Future<String> dailyReportByCountryCode(String country, String date) async {
     //2020-04-01
     var dailyReportUrl =
-        'https://covid-193.p.rapidapi.com/history?day=$date&country=$countryName';
-    var networkByCountryCode =
-        NetworkHelper(url: dailyReportUrl);
+        'https://covid-193.p.rapidapi.com/history?day=$date&country=$country';
+    var networkByCountryCode = NetworkHelper(url: dailyReportUrl);
     var dailyReportByCountryCode = await networkByCountryCode.getData();
     return dailyReportByCountryCode;
   }

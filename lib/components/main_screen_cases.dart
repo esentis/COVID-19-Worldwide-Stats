df1c637f1592d96f7a70f9556b803780f444cf7c
esentis <esentakos@yahoo.gr>
2020-09-19 12:55:08 +0300

    Circular results added on home results

--- lib/components/main_screen_cases.dart
+++ lib/components/main_screen_cases.dart
@@ -1,3 +1,4 @@
+import 'package:covid19worldwide/components/circular_result.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,83 +10,63 @@ class MainScreenCases extends StatelessWidget {
     this.newCases,
     this.newDeaths,
     this.overallCases,
-    this.casesText,
+    this.overallCasesText,
     this.newDeathsText,
     this.newCasesText,
   });
   final String overallCases;
   final String newCases;
   final String newDeaths;
-  final String casesText;
+  final String overallCasesText;
   final String newCasesText;
   final String newDeathsText;
   @override
   Widget build(BuildContext context) {
     return ElevatedCard(
       gradientColors: [
-        const Color(0xFF848ccf),
-        const Color(0xFF322f3d),
+        Colors.white,
+        Colors.red[900],
       ],
       shadowColor: Colors.black,
       elevation: 15,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
+      child: Expanded(
+        child: Wrap(
+          alignment: WrapAlignment.center,
           children: [
-            Center(
-              child: Text(
-                casesText,
-                style: GoogleFonts.gfsNeohellenic(
-                  fontSize: 25,
-                  color: const Color(0xFFebecf1).withOpacity(0.8),
-                ),
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: CircularResult(
+                color: const Color(0xfffe7171),
+                titleFontSize: 22,
+                contentFontSize: 22,
+                width: 150,
+                height: 100,
+                title: overallCasesText,
+                content: overallCases,
               ),
             ),
-            Center(
-              child: Text(
-                overallCases,
-                style: GoogleFonts.gfsNeohellenic(
-                  fontSize: 50,
-                  fontWeight: FontWeight.w600,
-                  color: const Color(0xFFebecf1).withOpacity(0.8),
-                ),
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: CircularResult(
+                color: Colors.orange[900],
+                titleFontSize: 22,
+                contentFontSize: 22,
+                width: 150,
+                height: 100,
+                title: newCasesText,
+                content: newCases,
               ),
             ),
-            Center(
-              child: Text(
-                newCasesText,
-                style: GoogleFonts.gfsNeohellenic(
-                  fontSize: 25,
-                  color: const Color(0xFFebecf1).withOpacity(0.8),
-                ),
-              ),
-            ),
-            Center(
-              child: Text(
-                kOverallNewCases,
-                style: GoogleFonts.gfsNeohellenic(
-                    fontSize: 50,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.orangeAccent.withOpacity(0.8)),
-              ),
-            ),
-            Center(
-              child: Text(
-                newDeathsText,
-                style: GoogleFonts.gfsNeohellenic(
-                  fontSize: 25,
-                  color: const Color(0xFFebecf1).withOpacity(0.8),
-                ),
-              ),
-            ),
-            Center(
-              child: Text(
-                kOverallNewDeaths,
-                style: GoogleFonts.gfsNeohellenic(
-                  fontSize: 50,
-                  fontWeight: FontWeight.w600,
-                  color: const Color(0xFFcf1b1b).withOpacity(0.8),
-                ),
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: CircularResult(
+                color: Colors.red[900],
+                titleFontSize: 22,
+                contentFontSize: 22,
+                width: 150,
+                height: 100,
+                title: newDeathsText,
+                content: newDeaths,
               ),
             ),
           ],
